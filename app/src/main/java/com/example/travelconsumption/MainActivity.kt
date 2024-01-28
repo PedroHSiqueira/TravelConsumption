e39a36b9486c110c6b0243f356bf4c39c25e1df8
Pedro Siqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-01-28 00:58:39 -0300

    Button Click Event

--- app/src/main/java/com/example/travelconsumption/MainActivity.kt
+++ app/src/main/java/com/example/travelconsumption/MainActivity.kt
@@ -2,19 +2,29 @@ package com.example.travelconsumption
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.travelconsumption.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_calculate) {
+            calculate()
+        }
     }
 
     private fun calculate() {
-
+        //Toast Notification
+        Toast.makeText(this, "Fui Clicado", Toast.LENGTH_SHORT).show()
     }
 
 }
